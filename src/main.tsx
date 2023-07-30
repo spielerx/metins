ada6e990bc2a0f0917b59d14b9eafd7cf90034dd
Roman <spielerua@gmail.com>
2023-07-30 19:15:09 +0300

    feat: add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import ThemeProvider from "providers/ThemeProvider.tsx";
+import { router } from "router";
+import { RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
 );
